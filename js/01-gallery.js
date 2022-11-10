75a543f33e3af4e24aab30abf6bd0396179ac5d5
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-11-11 00:48:08 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const refGallery = document.querySelector(".gallery");
+let instance = "";
+
+// ---- create gallery
+const createGalleryElements = ObjArr => {
+	const ElemArr = ObjArr.map(({ preview, original, description }) => {
+		return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+	});
+
+	insertGalleryElements(ElemArr);
+};
+
+const insertGalleryElements = ElemArr => {
+	refGallery.innerHTML = ElemArr.join("");
+};
+
+// ---- events, handlers
+
+const keyDownEscapeHandler = e => {
+	if (e.code !== "Escape") {
+		return;
+	}
+
+	instance.close();
+
+	window.removeEventListener("keydown", keyDownEscapeHandler);
+};
+
+const imgClickHandler = e => {
+	if (e.target.tagName !== "IMG") {
+		return;
+	}
+
+	e.preventDefault();
+
+	instance = basicLightbox.create(`
+    <img src=${e.target.dataset.source} width="800" height="600" >
+`);
+
+	instance.show();
+
+	window.addEventListener("keydown", keyDownEscapeHandler);
+};
+
+refGallery.addEventListener("click", imgClickHandler);
+
+//  ---- initialization
+createGalleryElements(galleryItems);
