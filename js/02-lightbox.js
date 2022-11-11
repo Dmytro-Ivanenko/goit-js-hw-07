7077169d7e5567fa3b7b3d099e3c56085eeb7fce
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-11-11 09:46:28 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const refGallery = document.querySelector(".gallery");
+
+// ---- create gallery
+const createGalleryElements = ObjArr => {
+	const ElemArr = ObjArr.map(({ preview, original, description }) => {
+		return `<a class="gallery__item" href=${original}>
+				<img class="gallery__image" src=${preview} alt= ${description}  />
+			</a>`;
+	});
+
+	insertGalleryElements(ElemArr);
+};
+
+const insertGalleryElements = ElemArr => {
+	refGallery.innerHTML = ElemArr.join("");
+};
+
+// ---- initialization
+
+createGalleryElements(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+	captions: true,
+	captionDelay: 250,
+	captionPosition: "bottom",
+	captionsData: "alt",
+});
